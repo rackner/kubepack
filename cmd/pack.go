a90eee0d9455ea5a0471daf4b40116a6dd428b54
Alex Raul <alextraul@gmail.com>
2020-05-04 17:06:20 -0400

    swap out tar for archivex and add variables

--- cmd/pack.go
+++ cmd/pack.go
@@ -16,20 +16,17 @@ limitations under the License.
 package cmd
 
 import (
-	"archive/tar"
-	"bufio"
-	"compress/gzip"
 	"context"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"log"
 	"os"
-	"path/filepath"
 	"strings"
 
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
+	"github.com/jhoonb/archivex"
 	copier "github.com/otiai10/copy"
 	"github.com/spf13/cobra"
 	yaml "gopkg.in/yaml.v2"
@@ -39,7 +36,10 @@ var Apps string
 var Output string
 
 type conf struct {
-	Images []string `yaml:"images"`
+	Images      []string `yaml:"images"`
+	OS          string   `yaml:"os"`
+	OSVersion   string   `yaml:"osversion"`
+	KubeVersion string   `yaml:"kubeversion"`
 }
 
 func pack() {
@@ -61,23 +61,27 @@ func pack() {
 	// Defer Removal
 	defer os.RemoveAll("./cluster")
 
+	// make directory for the "base" image
+	e3 := os.Mkdir("./cluster/apps", 0755)
+	check(e3)
+
+	// Build Base Image
+	buildAndSaveBase(c.OS, c.OSVersion, c.KubeVersion)
+
 	// Move application folder to tmp
 	e4 := copier.Copy(Apps, "./cluster/apps")
 	check(e4)
 	e5 := os.Mkdir("./cluster/images", 0755)
 	check(e5)
 
-	// Pull Images
+	// Pull Application Images
 	for _, value := range c.Images {
 		fmt.Println(value)
 		out, err := cli.ImagePull(ctx, value, types.ImagePullOptions{})
-
 		if err != nil {
 			panic(err)
 		}
-
 		io.Copy(os.Stdout, out)
-
 		arrayOfImages := []string{value}
 		fmt.Println(arrayOfImages)
 		// Save to Tarballs
@@ -85,25 +89,47 @@ func pack() {
 		check(err)
 		b, e := ioutil.ReadAll(outClose)
 		check(e)
-
 		// Create prefix if necessary
 		res1 := strings.SplitN(value, "/", -1)
-
 		e6 := os.Mkdir("./cluster/images/"+res1[0], 0755)
 		check(e6)
-
 		filename := "./cluster/images/" + value + ".tar"
 		fmt.Println(filename)
-		// Write Images to tar
 		ioutil.WriteFile(filename, b, 0755)
 	}
 
 	// Create Tarball
-	f, e3 := os.Create(Output)
-	check(e3)
-	w := bufio.NewWriter(f)
-	Tar("./cluster", w)
+	tar := new(archivex.TarFile)
+	tar.Create(Output)
+	tar.AddAll("./cluster", true)
+	tar.Close()
 
+}
+
+// Build the proper "base" image
+func buildAndSaveBase(OS string, OSVersion string, KubeVersion string) {
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		panic(err)
+	}
+	fmt.Print(cli.ClientVersion())
+	args := map[string]*string{
+		"KUBE_VERSION": &KubeVersion,
+		"OS_VERSION":   &OSVersion,
+	}
+	opt := types.ImageBuildOptions{
+		Dockerfile: "./starter-images/" + OS + "/Dockerfile",
+		BuildArgs:  args,
+	}
+	tar := new(archivex.TarFile)
+	tar.Create("/cluster/base.tar")
+	tar.Close()
+	dockerBuildContext, err := os.Open("/cluster/base.tar")
+	defer dockerBuildContext.Close()
+	_, err = cli.ImageBuild(context.Background(), dockerBuildContext, opt)
+	if err == nil {
+		fmt.Printf("Error, %v", err)
+	}
 }
 
 func (c *conf) getConf() *conf {
@@ -149,132 +175,4 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// packCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-}
-
-// Tar takes a source and variable writers and walks 'source' writing each file
-// found to the tar writer; the purpose for accepting multiple writers is to allow
-// for multiple outputs (for example a file, or md5 hash)
-func Tar(src string, writers ...io.Writer) error {
-
-	// ensure the src actually exists before trying to tar it
-	if _, err := os.Stat(src); err != nil {
-		return fmt.Errorf("Unable to tar files - %v", err.Error())
-	}
-
-	mw := io.MultiWriter(writers...)
-
-	tw := tar.NewWriter(mw)
-	defer tw.Close()
-
-	// walk path
-	return filepath.Walk(src, func(file string, fi os.FileInfo, err error) error {
-		fmt.Println(fi.Name())
-		// return on any error
-		if err != nil {
-			return err
-		}
-
-		// return on non-regular files (thanks to [kumo](https://medium.com/@komuw/just-like-you-did-fbdd7df829d3) for this suggested update)
-		if !fi.Mode().IsRegular() {
-			return nil
-		}
-
-		// create a new dir/file header
-		header, err := tar.FileInfoHeader(fi, fi.Name())
-		if err != nil {
-			return err
-		}
-
-		// update the name to correctly reflect the desired destination when untaring
-		header.Name = strings.TrimPrefix(strings.Replace(file, src, "", -1), string(filepath.Separator))
-
-		// write the header
-		if err := tw.WriteHeader(header); err != nil {
-			return err
-		}
-
-		// open files for taring
-		f, err := os.Open(file)
-		if err != nil {
-			return err
-		}
-
-		// copy file data into tar writer
-		if _, err := io.Copy(tw, f); err != nil {
-			return err
-		}
-
-		// manually close here after each file operation; defering would cause each file close
-		// to wait until all operations have completed.
-		f.Close()
-
-		return nil
-	})
-}
-
-// Untar takes a destination path and a reader; a tar reader loops over the tarfile
-// creating the file structure at 'dst' along the way, and writing any files
-func Untar(dst string, r io.Reader) error {
-
-	gzr, err := gzip.NewReader(r)
-	if err != nil {
-		return err
-	}
-	defer gzr.Close()
-
-	tr := tar.NewReader(gzr)
-
-	for {
-		header, err := tr.Next()
-
-		switch {
-
-		// if no more files are found return
-		case err == io.EOF:
-			return nil
-
-		// return any other error
-		case err != nil:
-			return err
-
-		// if the header is nil, just skip it (not sure how this happens)
-		case header == nil:
-			continue
-		}
-
-		// the target location where the dir/file should be created
-		target := filepath.Join(dst, header.Name)
-
-		// the following switch could also be done using fi.Mode(), not sure if there
-		// a benefit of using one vs. the other.
-		// fi := header.FileInfo()
-
-		// check the file type
-		switch header.Typeflag {
-
-		// if its a dir and it doesn't exist create it
-		case tar.TypeDir:
-			if _, err := os.Stat(target); err != nil {
-				if err := os.MkdirAll(target, 0755); err != nil {
-					return err
-				}
-			}
-
-		// if it's a file create it
-		case tar.TypeReg:
-			f, err := os.OpenFile(target, os.O_CREATE|os.O_RDWR, os.FileMode(header.Mode))
-			if err != nil {
-				return err
-			}
-
-			// copy over contents
-			if _, err := io.Copy(f, tr); err != nil {
-				return err
-			}
-
-			// manually close here after each file operation; defering would cause each file close
-			// to wait until all operations have completed.
-			f.Close()
-		}
-	}
 }
